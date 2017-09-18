e195f11a78fcc47fca10bc6cd4748d6ad569e59c
Daniel <huanchicayd@gmail.com>
2017-09-18 08:41:23 -0300

    ''

---
+++ header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bontec, Economia de água, Conserto de aquecedores, Reformas</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <a href="#">
+                        <img src="logo.svg" alt="Logotipo da Bontec">
+                    </a>
+                </div>
+                <div class="col-9">
+                    <nav>
+                        <ul>
+                            <li>Sobre</li>
+                            <li>Planos</li>
+                            <li>Serviços</li>
+                            <li>Obras</li>
+                            <li>Loja</li>
+                            <li>Contato</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,124 @@
+<?php include('header.php') ?>
+
+<main>
+    <div class="home-slider">
+        <div class="container">
+            <div class="home-slider__content">
+                <div class="home-slider__content__box">
+                    <h1>Síndico evite o desperdício!</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="voce-sabia">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Você sabia?</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+
+                </div>
+                <div class="col-4">
+
+                </div>
+                <div class="col-4">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="saude-predial">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Saúde Predial Plus Plano de Proteção Hidráulica</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="saude-predial__video">
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <p>
+
+                    </p>
+                </div>
+                <div class="col-6">
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="saude-predial__servicos">
+
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="saude-predial__obras">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="beneficios">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Benefícios de ser cliente Bontec</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hidrometro">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="hidrometro__animacao">
+
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="hidrometro__texto">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include('header.php') ?>
