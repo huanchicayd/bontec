0031ada5de0e9b3a51822ae757ac1351c0f4c6b4
Daniel Huanchicay <huanchicayd@gmail.com>
2017-09-25 17:05:50 -0300

    Implementação da página Planos

--- dev/planos.php
+++ dev/planos.php
@@ -1,7 +1,110 @@
 <?php include('header.php') ?>
 
-    <main>
+<main class="saude-predial-interna">
+    <div class="banner banner__planos"></div>
+    <div class="saude-predial-interna__descricao">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Saúde Predial Plus</h2>
+                    <h5 class="section-header__subtitulo">Soluções para economia de água em seu condomínio</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>
+                        A <strong>Bontec</strong> desenhou um modelo inovador de erviçoes, onde <strong>diagnosticamos as necessidades de cada prédio</strong> e trabalhamos em conjunto com moradres e síndicos em prol da cultura de economia de água e a redução dos custos.
+                    </p>
+                    <p>
+                        Na <strong>Bontec - Saúde Predial Plus</strong>, temos o objetivo principal de solucionar uma das maiores preocupações dos síndico: <strong>A redução do valor da conta de água em seu condomínio</strong>.
+                    </p>
+                </div>
+                <div class="col-lg-6">
+                    <p>
+                        Estão inclusos no nosso pacote todos os serviços corretivos no sistema hidráulico de <strong>todos os apartaments e de toda parte comum do condomínio</strong>, incluindo o material necessário.
+                    </p>
+                    <p>
+                        Além do melhor custo benefício em relação a contratação dos serviços avulsos, <strong>obtenha segurança através de funcionário exclusivos</strong> devidamente uniformizado, que somente circulam pelo condomínio se autorizados pela portaria.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="saude-predial-plano">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h5 class="saude-predial__subtitulo">Oferecemos diversos planos para atender suas prioridades</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="saude-predial-plano__tabela">
+                        <table>
+                            <tr>
+                                <td class="table-header">Para condomínios residenciais e comerciais</td>
+                            </tr>
+                            <tr class="table-content">
+                                <td>
+                                    Serviços incluídos
+                                    <ul>
+                                        <li>Consertos de válvulas de descarga;</li>
+                                        <li>Consertos de caixas acopladas;</li>
+                                        <li>Substituição de rabichos (PVC);</li>
+                                        <li>Conserto ou substituição de torneiras, castelos de torneiras/registros e duchinhas higiênicas;</li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="saude-predial-video">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
 
-    </main>
+                </div>
+                <div class="col-lg-4">
+                    Veja como funciona
+                    Saúde Predial Plus
+                    Plano de economia de água para condomínios
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="outros-planos">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Outros Planos</h2>
+                    <h5 class="section-header__subtitulo">Oferecemos diversos planos para atender suas prioridades</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table>
+                        <thead></thead>
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php include('footer.php') ?>
