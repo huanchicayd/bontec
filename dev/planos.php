5a9f2afcda249eef8d91156ce32baa27c7ffd51f
Daniel Huanchicay <huanchicayd@gmail.com>
2017-09-26 12:52:13 -0300

    Termino da pagina planos

--- dev/planos.php
+++ dev/planos.php
@@ -13,7 +13,7 @@
             <div class="row">
                 <div class="col-lg-6">
                     <p>
-                        A <strong>Bontec</strong> desenhou um modelo inovador de erviçoes, onde <strong>diagnosticamos as necessidades de cada prédio</strong> e trabalhamos em conjunto com moradres e síndicos em prol da cultura de economia de água e a redução dos custos.
+                        A <strong>Bontec</strong> desenhou um modelo inovador de erviçoes, onde <strong>diagnosticamos as necessidades de cada prédio</strong> e trabalhamos em conjunto com moradores e síndicos em prol da cultura de economia de água e a redução dos custos.
                     </p>
                     <p>
                         Na <strong>Bontec - Saúde Predial Plus</strong>, temos o objetivo principal de solucionar uma das maiores preocupações dos síndico: <strong>A redução do valor da conta de água em seu condomínio</strong>.
@@ -21,7 +21,7 @@
                 </div>
                 <div class="col-lg-6">
                     <p>
-                        Estão inclusos no nosso pacote todos os serviços corretivos no sistema hidráulico de <strong>todos os apartaments e de toda parte comum do condomínio</strong>, incluindo o material necessário.
+                        Estão inclusos no nosso pacote todos os serviços corretivos no sistema hidráulico de <strong>todos os apartamentos e de toda parte comum do condomínio</strong>, incluindo o material necessário.
                     </p>
                     <p>
                         Além do melhor custo benefício em relação a contratação dos serviços avulsos, <strong>obtenha segurança através de funcionário exclusivos</strong> devidamente uniformizado, que somente circulam pelo condomínio se autorizados pela portaria.
@@ -93,12 +93,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
-
+                    <div class="saude-predial-video__video">
+                        <iframe src="https://www.youtube.com/embed/s4qiZkKTFHU?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                    </div>
                 </div>
                 <div class="col-lg-4">
-                    Veja como funciona
-                    Saúde Predial Plus
-                    Plano de economia de água para condomínios
+                    <div class="saude-predial-video__texto">
+                        <h6>Veja como funciona</h6>
+                        <h3>Saúde Predial Plus</h3>
+                        <h4>Plano de economia de água para condomínios</h4>
+                    </div>
                 </div>
             </div>
         </div>
@@ -113,16 +117,138 @@
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <table>
-                        <thead></thead>
-                        <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <div class="outros-planos__tabela">
+                        <div class="tabela">
+                            <div class="tabela__header">
+                                <h4>Para empresas</h4>
+                            </div>
+                            <div class="tabela__body">
+                                <div>
+                                    <h4>Serviços incluídos</h4>
+                                    <ul>
+                                        <li>Consertos de válvulas de descarga;</li>
+                                        <li>Consertos de caixas acopladas;</li>
+                                        <li>Substituição de rabichos (PVC);</li>
+                                        <li>Consertos ou substituição de torneiras, castelos de torneiras/registros e duchinhas higiênicas;</li>
+                                        <li>Consertos, regulagens ou substituição de bóias e automáticos de cisternas e caixas d'água;</li>
+                                        <li>Consultoria hidráulica - Para identificação de vazamentos em tubulações de incêndio, de alimentação de cisternas e caixas d'água;</li>
+                                        <li>Acompanhamento regular da conta de água, de acrodo com os gráficos de controle da empresa.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4>Materiais incluídos</h4>
+                                    <ul>
+                                        <li>Reparo de válvulas de descarga;</li>
+                                        <li>Bóia de caixa acoplada;</li>
+                                        <li>Acionador (simples acionamento);</li>
+                                        <li>Cabeçote;</li>
+                                        <li>Obturador;</li>
+                                        <li>Torre de entrada/saída para caixas acopladas (simples acionamento);</li>
+                                        <li>Carrapetas/vedantes;</li>
+                                        <li>Juntas de vedação, fibras, fita teflon e massa zarcão.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4>Prioridades em foco</h4>
+                                    <ul>
+                                        <li>Economia direta de água/luz;</li>
+                                        <li>Comodidade para marcação das visitas;</li>
+                                        <li>Segurança;</li>
+                                        <li>Melhor custo x benefício.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="tabela__footer">
+                                Formas de pagamento: Boleto bancário || Contrato anual
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="outros-planos__tabela">
+                        <div class="tabela">
+                            <div class="tabela__header">
+                                <h4>Para unidades individuais de apartamentos e casas*</h4>
+                            </div>
+                            <div class="tabela__body">
+                                <div>
+                                    <h4>Serviços incluídos</h4>
+                                    <ul>
+                                        <li>Consertos de válvulas de descarga;</li>
+                                        <li>Consertos de caixas acopladas;</li>
+                                        <li>Substituição de rabichos (PVC);</li>
+                                        <li>Consertos ou substituição de torneiras, castelos de torneiras/registros e duchinhas higiênicas;</li>
+                                        <li>Consertos, regulagens ou substituição de bóias e automáticos de cisternas e caixas d'água;</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4>Materiais incluídos</h4>
+                                    <ul>
+                                        <li>Reparo de válvulas de descarga;</li>
+                                        <li>Bóia de caixa acoplada;</li>
+                                        <li>Acionador (simples acionamento);</li>
+                                        <li>Cabeçote;</li>
+                                        <li>Obturador;</li>
+                                        <li>Torre de entrada/saída para caixas acopladas (simples acionamento);</li>
+                                        <li>Rabichos em PVC;</li>
+                                        <li>Carrapetas/vedantes;</li>
+                                        <li>Juntas de vedação, fibras, fita teflon e massa zarcão.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4>Prioridades em foco</h4>
+                                    <ul>
+                                        <li>Economia direta de água/luz;</li>
+                                        <li>Comodidade para marcação das visitas;</li>
+                                        <li>Segurança;</li>
+                                        <li>Melhor custo x benefício.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="tabela__footer">
+                                Formas de pagamento: Boleto bancário || Contrato anual || *Dispensa contratação de todo o condomínio
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="outros-planos__tabela">
+                        <div class="tabela">
+                            <div class="tabela__header">
+                                <h4>Para aquecedores e boilers</h4>
+                            </div>
+                            <div class="tabela__body">
+                                <div>
+                                    <h4>Serviços incluídos</h4>
+                                    <ul>
+                                        <li>Consertos, regulagens ou substituição de bóias e automáticos de cisternas e caixas d'água.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4>Materiais incluídos</h4>
+                                    <ul>
+                                        <li>Diafragmas;</li>
+                                        <li>Juntas de vedação, fibras e fita teflon.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4>Prioridades em foco</h4>
+                                    <ul>
+                                        <li>Comodidade para marcação das visitas;</li>
+                                        <li>Segurança;</li>
+                                        <li>Melhor custo x benefício.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="tabela__footer">
+                                Formas de pagamento: Boleto bancário || Contrato anual
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
